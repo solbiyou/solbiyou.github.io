6b8b83afb7b14477ddb4a5663383e39a63c09662
Solbi You <solbi.you@sjsu.edu>
2020-09-27 19:58:02 -0700

    add transparency when scrolled down in js

--- main.js
+++ main.js
@@ -33,9 +33,22 @@ contactBtn.addEventListener('click', (event) => {
   scrollIntoView('#contact');
 });
 
+// make home slowly fade to tranparent as scrolled down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({
     behavior: 'smooth'
   });
 }
+
