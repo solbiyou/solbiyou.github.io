9984c09013e4dc7ae5117d69332e158278d3c191
Solbi You <solbi.you@sjsu.edu>
2020-09-27 18:48:29 -0700

    add contact button in js

--- main.js
+++ main.js
@@ -5,11 +5,37 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`NavbarHeight: ${navbarHeight}`);
+  // console.log(window.scrollY);
+  // console.log(`NavbarHeight: ${navbarHeight}`);
   if(window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
         navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  scrollIntoView(link);
+
+});
+
+//click on contact me to move to contact part
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
